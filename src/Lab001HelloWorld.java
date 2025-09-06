470a826a13a9b799fadc59d71aa7274f3ce49c1e
“bhagyagit config --global user.email “bhagyarani2699@gmail.com <bhagyarani2699@gmail.com>
2025-09-06 13:04:44 +0530

    Init change commit

--- src/Lab001HelloWorld.java
+++ src/Lab001HelloWorld.java
@@ -1,6 +1,10 @@
+
+
 public class Lab001HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
-        System.out.println("Geetha the Queen");
+
+
     }
 }
