250cfe57df43d6a4afbcbe4ad570bd6f72c8e9ed
“bhagyagit config --global user.email “bhagyarani2699@gmail.com <bhagyarani2699@gmail.com>
2025-09-09 21:53:16 +0530

    Init change commit

---
+++ src/PrintNumbers.java
@@ -0,0 +1,9 @@
+public class PrintNumbers {
+
+    public static void main(String[] args) {
+
+        for (int i = 5; i <= 10; i++) {
+            System.out.println("My number is " + i);
+        }
+    }
+}
