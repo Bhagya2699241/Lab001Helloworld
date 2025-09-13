dc41002be2af01c25446a80add7cd88110738a65
Bhagya2699241 <bhagyarani2699@gmail.com>
2025-09-13 11:28:22 +0530

    Update Lab001HelloWorld.java
    added lines

--- src/Lab001HelloWorld.java
+++ src/Lab001HelloWorld.java
@@ -4,6 +4,11 @@ public class Lab001HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 4");
+        
 
 
     }
